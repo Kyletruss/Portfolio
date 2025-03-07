980af9f4bf14ec10ac79a2414fa1e841ef9e2d9f
Kyle Truss <kyletruss@gmail.com>
2025-03-06 19:19:04 -0500

    Built the responsive grid

--- sorting-visualizer/src/App.tsx
+++ sorting-visualizer/src/App.tsx
@@ -1,10 +1,20 @@
 
 
-import { Button, ButtonGroup } from "@chakra-ui/react"
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return(
-    <Button colorPalette='red' rounded="md">Click me</Button>
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`
+    }}>
+      <GridItem area='nav' bg='coral'>Nav</GridItem>
+
+      <GridItem area='aside' bg='gold' display={{ base: "none", lg: "block" }}>Aside</GridItem>
+
+
+      <GridItem area='main' bg='dodgerblue'>Main</GridItem>
+    </Grid>
   )
 
 }
